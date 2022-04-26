73830160454da51829335e6d4d522787f74b5c30
Carlos Reis <carlos.henriquer287@gmail.com>
2022-04-26 15:20:51 -0300

    FEAT: config cors.

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ const app = express();
 const vehicleRouter = require("./routes/vehicle");
 const bp = require("body-parser");
 
+const cors = require("cors");
+const corsOptions = {
+  origin: "http://localhost:3001",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+
 app.use("/vehicle", vehicleRouter);
 
 app.use(bp.json());
